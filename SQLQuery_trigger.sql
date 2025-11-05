07bcbc2f43b995551cfdc896866becfc142d7108
Bellos0 <118614991+Bellos0@users.noreply.github.com>
2025-11-05 23:47:42 +0700

    dynamic store value, calculated and judgment rank
    tinh toan diem trung binh va jugdment rank cho bang diem
    ------
    luu nhieu gia tri vao 1 cot ma k can tao them nhieu cot moi

--- SQLQuery_trigger.sql
+++ SQLQuery_trigger.sql
@@ -133,6 +133,136 @@ delete from test1 where value11<3
 select FORMAT(GETDATE(),'dd/MM/yyyy')
 
 
+-- thu nghiem trigger tinh toan tu dong trong database.
+--yeu cau nhap diem kiem tra 15p va diem kiem tra 60p tinh ra diem trung binh roi cho dang xep loai
+CREATE TABLE scoreboard
+(
+    stt int identity(1,1),
+    score15 char(5),
+	score60 char(5),
+	avgScore char(5),
+	rank_ char(5),
+	constraint PK_stt primary key (stt)
+);
+alter table scoreboard
+alter column score15 float
+go
+alter table scoreboard
+alter column score60 float
+go
+alter table scoreboard
+alter column avgScore float
+go
+alter table scoreboard
+alter column score15 float
+go;
+-- trigger tu dong tinh toan va update so vao trong cot avgscore va rank_
+
+create trigger UT_calculateScore
+on scoreboard 
+after insert, update
+as
+BEGIN
+	set nocount on;
+-- avgScore = score15*0.3 + score60*0.7
+-- rank_ nhu ngay dai hoc
+	update sb 
+	set
+	avgScore = i.score15*0.3 + i.score60*0.7,
+	rank_= case
+				when (i.score15 * 0.3 + i.score60 * 0.7) <= 8.5  and (i.score15 * 0.3 + i.score60 * 0.7)  <10.0  then 'A'
+				when (i.score15 * 0.3 + i.score60 * 0.7) <= 7.0  and (i.score15 * 0.3 + i.score60 * 0.7)  <8.5 then 'B'
+				when (i.score15 * 0.3 + i.score60 * 0.7) <= 5.0 and (i.score15 * 0.3 + i.score60 * 0.7)  <7.0  then 'C'
+				when (i.score15 * 0.3 + i.score60 * 0.7) <= 3.0  and (i.score15 * 0.3 + i.score60 * 0.7)  <5.0  then 'D'
+				else 'F'
+			end
+	from scoreboard sb
+	inner join inserted i on sb.stt = i.stt;
+END;
+go
+---------------------------
+alter trigger UT_calculateScore
+on scoreboard 
+after insert, update
+as
+BEGIN
+	set nocount on;
+-- avgScore = score15*0.3 + score60*0.7
+-- rank_ nhu ngay dai hoc
+	update sb -- doan nay dung cau lenh update cua sql update tablename set val1='', val2='' from talename. 
+	set
+	avgScore = i.score15*0.3 + i.score60*0.7, -- i la viet tat cho inserted alias
+	rank_= case
+				when (i.score15 * 0.3 + i.score60 * 0.7) >= 8.5  and (i.score15 * 0.3 + i.score60 * 0.7)  <10.0  then 'A'
+				when (i.score15 * 0.3 + i.score60 * 0.7) >= 7.0  and (i.score15 * 0.3 + i.score60 * 0.7)  <8.5 then 'B'
+				when (i.score15 * 0.3 + i.score60 * 0.7) >= 5.0 and (i.score15 * 0.3 + i.score60 * 0.7)  <7.0  then 'C'
+				when (i.score15 * 0.3 + i.score60 * 0.7) >= 3.0  and (i.score15 * 0.3 + i.score60 * 0.7)  <5.0  then 'D'
+				else 'F'
+			end
+	from scoreboard sb -- tuong tu voi truong hop cua inserted
+	inner join inserted i on sb.stt = i.stt; -- khia bao inserted viet tat la i
+END;
+go
+
+
+---------------------------
+-- tao trigger khi update 1 gia tri score15 thi gia tri score15 cu giu nguyen, gia tri moi duoc luu vao 1 cot moi score15_1, nhung la tiep theo la score15_i
+
+create trigger UT_CreateNewColumn
+on test1
+after update
+as
+
+BEGIN
+
+ set nocount on;
+ --
+ -- hang muc them gia tri
+ insert test1( ) 
+ select 
+ -- new value 
+ from inserted 
+END
+go
 
 
 
+
+-- test trigger UT_calculateScore
+insert into scoreboard (score15,score60) values (8.2,9.1);
+go
+select * from scoreboard;
+go
+
+create table testStore
+(
+id int identity(1,1),
+val1 varchar(max),
+val2 varchar(max)
+constraint PK_id_testStore primary key (id)
+
+)
+
+---
+-- trigger dynamic value khi update cac gia tri them vao ma khong can phai tao them cot trong database
+create trigger UT_StoreDynamicVal1
+on testStore
+for update
+as
+
+BEGIN
+	if update(val1)
+
+		BEGIN
+		update testStore 
+		set val1 = d.val1+','+i.val1
+		 from inserted i join deleted d on d.id=i.id
+		 where testStore.id=i.id
+		END
+END
+
+
+insert into testStore values ('aa','b')
+update testStore set val1 = '22' from testStore
+go
+select * from testStore 
